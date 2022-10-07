0e6dba643c2216045586f2123e289e4f62d0f8f1
Cheng Li <Cheng.Li@mindgeek.com>
2022-10-08 00:41:53 +0800

    Chrome

---
+++ Chrome/107/permission-policy/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+
+app.get('/', function (req, res) {
+    console.log("root page")
+    res.header('Permissions-Policy', 'fullscreen=(self "http://local.test.com:3000")');
+    // res.header('Permissions-Policy', 'fullscreen=(self "http://*.test.com:3000")');
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.use(express.static('public'));
+
+app.listen(3000)
