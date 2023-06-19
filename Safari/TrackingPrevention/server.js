421132b6ef8c9eaf3c5f82baaecf068ba9a4f950
李程 <lichengac@gmail.com>
2023-06-19 19:38:43 -0400

    wip

--- Safari/TrackingPrevention/server.js
+++ Safari/TrackingPrevention/server.js
@@ -4,10 +4,20 @@ const app = express()
 const port = 3000
 const path = require('path')
 
+const bookList = require('./books.json')
+const bookMap = new Map(bookList.map(it => ([it.id, it])))
+
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
 })
 
+app.get('/books', (req, res) => {
+    console.log(req.query)
+    console.log(bookList)
+    res.render('books', { title: 'books', books: bookList, message: 'pug' })
+})
 
 app.use(express.static('public'))
 
